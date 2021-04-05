4bec4d0d78b6ae65968c4b6c29ce6d7f87343e37
Tim van Dijen <tim.dijen@minbzk.nl>
2021-04-05 18:10:11 +0200

    Remove ldap-reference

--- tests/autoload.php
+++ tests/autoload.php
@@ -6,7 +6,7 @@ $projectRoot = dirname(__DIR__);
 require_once($projectRoot . '/vendor/autoload.php');
 
 // Symlink module into saml2 vendor lib so that templates and urls can resolve correctly
-$linkPath = $projectRoot . '/vendor/simplesamlphp/saml2/modules/ldap';
+$linkPath = $projectRoot . '/vendor/simplesamlphp/saml2/modules/spid';
 if (file_exists($linkPath) === false) {
     echo "Linking '$linkPath' to '$projectRoot'\n";
     symlink($projectRoot, $linkPath);
